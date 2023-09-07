bf2445a8d422e1769bfbac6148bd100a36f73e43
weiyaqi1123 <3289834600@qq.com>
2023-09-07 21:44:03 +0800

    学生仓库信息

---
+++ push.sh
@@ -0,0 +1,9 @@
+git config --global user.name 'weiyaqi1123'
+git cogfig --global user.email '3289834600@qq.com'
+git config --global http.sslverify false
+git add .
+git remote add origin https://github.com/weiyaqi1123/3289834600.git
+git branch -M main
+git push -u origin main
+git commit -m'自动推送'
+git push
