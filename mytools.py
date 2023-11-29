b084c1b8885d685b85f443107e6bafc73a2fc443
weiyaqi1123 <3289834600@qq.com>
2023-11-29 13:17:23 +0800

    作业六

--- mytools.py
+++ mytools.py
@@ -4,7 +4,6 @@ from pyreadstat import pyreadstat
 import matplotlib.pyplot as plt
 
 
-
 def 绘制饼图(数据表, 变量):
  # 定义数据  
   labels = ['A', 'B', 'C', 'D'] 
@@ -116,5 +115,21 @@ def 制作交叉表(数据表, 自变量, 因变量):
     return pd.crosstab(数据表[自变量], 数据表[因变量], normalize='columns', margins=True)
 
 
+import pandas as pd
+from scipy import stats
 
-
+def 单变量均值函数的区间估计(file_path,confidence_level):
+    file_path ="文件所在位置及名称"
+    df_movies = pd.read_csv(file_path)
+    # 计算均值和标准误差
+    mean = df_movies['average'].mean()
+    std_error = stats.sem(df_movies['average'])
+# 设定置信水平
+    confidence_level = 0.95
+# 设定自由度
+    自由度 = len(df_movies['average']) - 1
+# 计算置信区间
+    confidence_interval = stats.t.interval(confidence_level, 自由度, loc=mean, scale=std_error)
+# 输出结果
+    print(F"均值：{mean: .2f}")
+    print(F"均值在置信水平{confidence_level}下的置信区间为：", confidence_interval)
